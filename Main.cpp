e4d5b60dce90f71659d6b52cbca93c33b4053d07
Alexander Alava <AlexanderAlava@Alexanders-MacBook-Pro.local>
2016-11-21 19:34:54 -0500

    Cleaning driver and pushing back original text file

--- Main.cpp
+++ Main.cpp
@@ -35,65 +35,44 @@ int main()
   string standing[100];
 
   //Object of inputFileStream named inFile. Will handle file input.
-  ifstream inAlex;
+  ifstream inStudent;
   //Open the file
-  inAlex.open("Students.txt");
+  inStudent.open("Students.txt");
 
-  if(inAlex.fail())
+  if(inStudent.fail())
   {
     cerr << "Error while opening Students.txt" << endl;
     //Terminates the program unsuccesfully
     exit(1);
   }
 
-  inAlex.seekg(0l);
-  while(!inAlex.eof())
+  inStudent.seekg(0l);
+  while(!inStudent.eof())
   {
-    getline(inAlex, studentsArray[i], ',');
+    getline(inStudent, studentsArray[i], ',');
     i++;
   }
 
-  inAlex.close();
-
-
-
-  /*
-  cout << studentsArray[5] << endl;
-  cout << studentsArray[0] << endl;
-  cout << studentsArray [10] << endl;
-  */
-
-/*
-  if (studentsArray[8] == "U")
-  {
-    Undergrad a(studentsArray[0], studentsArray[4], studentsArray[12]);
-
-  }
-
-*/
-
-
-  //Some testing with reading from a file below. Uncomment if you want to test.
-
+  inStudent.close();
 
   //Object of inputFileStream named inFile. Will handle file input.
-  ifstream inFile;
+  ifstream inTeacher;
   //Open the file
-  inFile.open("Teachers.txt");
+  inTeacher.open("Teachers.txt");
 
   //Check for errors when opening the file
-  if(inFile.fail())
+  if(inTeacher.fail())
   {
     cerr << "Error while opening Teachers.txt" << endl;
     //Terminates the program unsuccesfully
     exit(1);
   }
 
-  inFile.seekg(0l); //Positions file-position pointer at begining of file
+  inTeacher.seekg(0l); //Positions file-position pointer at begining of file
 
-  while(!inFile.eof())
+  while(!inTeacher.eof())
     {
-      getline(inFile, teachersArray[i], ',');
+      getline(inTeacher, teachersArray[i], ',');
       h++;
     }
 
@@ -132,7 +111,7 @@ int main()
   }
 
   //close the file after done using
-  inFile.close();
+  inTeacher.close();
 
   for (int k = 0; k <= 3; k++)
   {
@@ -145,71 +124,6 @@ int main()
       studentVec[k] = new Grad(names[k], id[k], standing[k]);
     }
   }
-
-
-/* I tested the above to verify that I could read from a file and use a comma
-*  as the separator between items that we're looking for. Example, comma
-*  separates TeacherID,FullName,Age. In this order within the text file.
-*  The code is commented out above and can be tested by just uncommenting it.
-*  it works for the file I have included "Teachers.txt".
-*/
-
-
-/*
-  do
-  {
-    // Prompting for and reading in user input for choice //
-    cout << "\nPlease enter your selection:\n" << endl;
-    cout << "~ Enter s to add a new student to the system.";
-    cout << "~ Enter t to add a new teacher to the system.";
-    cout << "~ Enter d to add a new department to the system.";
-    cout << "~ Enter c to add a new course to the system.";
-    cout << "\nOtherwise, please enter character to terminate the program.\n";
-    cout << "\nYour selection: ";
-    cin >> generalChoice;
-
-    // Converting choice to lowercase //
-    tolower(generalChoice);
-
-    // Establishing a switch statement to account for all different options //
-    switch (generalChoice)
-    {
-      // Case for student //
-      case 's':
-
-        cout << "\nYou have selected to add a new student! Please select what kind of student.";
-        cout << "Enter u for undergraduate or g for graduate: ";
-        cin >> studentChoice;
-
-
-
-        break;
-
-      // Case for teacher //
-      case 't':
-
-        break;
-
-      // Case for department //
-      case 'd':
-
-        break;
-
-      // Case for course //
-      case 'c':
-
-        break;
-
-      // Case for termination //
-      default:
-
-        generalChoice = 0;
-        break;
-    }
-
-  }
-  while (generalChoice);
-*/
 
 
   //succesfull program termination
